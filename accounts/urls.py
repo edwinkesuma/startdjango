492ac71b9e8ec7fd556094b163dce1e8f2d48f49
origin <https://github.com/edwinkesuma/GetEat>
2021-10-12 20:08:47 +0700

    first commit

---
+++ accounts/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.loginPage, name='login'),
+    path('register/', views.registerPage, name='register'),
+    path('logout/', views.logoutUser, name='logout'),
+
+    path('', views.dashboard, name='home'),
+    path('customer/<str:pk>/', views.customer, name='customer'),
+    path('products/', views.products, name='products'),
+    path('user/', views.userPage, name='user-page'),
+
+    path('create_order/<str:pk>/', views.createOrder, name="create_order"),
+    path('update_order/<str:pk>/', views.updateOrder, name="update_order"),
+    path('delete_order/<str:pk>/', views.deleteOrder, name="delete_order"),
+]
